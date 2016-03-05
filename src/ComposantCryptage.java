73655db9789be78216a4e447b2661ae449d0d8a7
Alan Jaouen <alanJaouen@users.noreply.github.com>
2016-03-05 16:12:27 +0000

    ajout du projet modelio/ecipse

---
+++ src/ComposantCryptage.java
@@ -0,0 +1,8 @@
+import com.modeliosoft.modelio.javadesigner.annotations.objid;
+
+@objid ("52ffa1bb-c284-472c-b750-b35349f6460c")
+public class ComposantCryptage extends Composant {
+    @objid ("8624c751-3a28-416f-97f9-6802bf5c29ee")
+    public char[] lettresRotor;
+
+}
---
+++ src/Reflecteur.java
@@ -0,0 +1,8 @@
+import com.modeliosoft.modelio.javadesigner.annotations.objid;
+
+@objid ("55ced5e2-30c5-4efa-a246-f1911674904b")
+public class Reflecteur extends ComposantCryptage {
+    @objid ("f743b995-4e14-4e97-8bf4-483dc4e8c277")
+    public synchronized native char encoder(char c);
+
+}
