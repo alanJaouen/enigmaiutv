397c832e06e03fdb6c7d33c17c7f80584c929b9e
corentin-dem <corentin-dem@44c46b43-e275-4c20-bec9-bf31a1ff7110>
2016-03-07 17:09:33 +0000

    class Cable + debut Rotor

--- projet/Enigma/src/Client.java
+++ projet/Enigma/src/Client.java
@@ -1,9 +1,7 @@
-import com.modeliosoft.modelio.javadesigner.annotations.objid;
 
-@objid ("fc739fe0-0739-4f5a-9f78-1057a3b07659")
-public class Client {
-    @objid ("0a01d7f8-b349-4f16-be11-eae4d9f02360")
-    public static void main(String[] args) {
+public class Client 
+{
+    public static void main(String[] args) 
+    {
     }
-
 }
--- projet/Enigma/src/Rotor.java
+++ projet/Enigma/src/Rotor.java
@@ -1,26 +1,44 @@
-import com.modeliosoft.modelio.javadesigner.annotations.objid;
 
-@objid ("6141490c-4679-44b7-ae18-7163cf71598b")
-public class Rotor extends ComposantCryptage {
-    @objid ("303eba76-0291-4995-839c-b9522eeb45d7")
+public class Rotor extends ComposantCryptage
+{
     public int positionRotor;
 
-    @objid ("cd9ca03d-8a0f-4997-a0ff-edc0917b4ce3")
     public int positionInitiale;
 
-    @objid ("4b38cc53-b3c6-4784-bc2c-58de4fccfe79")
     public int positionCourante;
 
-    @objid ("11f7c4d7-60bd-4988-86e9-ae94cbd540e0")
-    public void positionInitiale(char c) {
+    public Rotor()
+    {
+    	this.positionRotor = 0;
+    	this.positionInitiale = 0;
+    	this.positionCourante = 0;
+    }
+    
+    public Rotor(int pRotor, int pInit, int pCourante)
+    {
+    	this.positionRotor = pRotor;
+    	this.positionInitiale = pInit;
+    	this.positionCourante = pCourante;
+    }
+    
+    public void positionInitiale(char c) 
+    {
     }
 
-    @objid ("8620f804-6303-403a-90a7-8ffe59496b90")
-    public synchronized char encoder(char c, int sens) {
+    public char encoder(char c, int sens) 
+    {
     }
 
-    @objid ("30824316-60ed-499d-ae4f-732e45e06f58")
-    public void tourner() {
+    public void tourner() 
+    {
     }
+
+
+
+	@Override
+	public char encoder(char c) 
+	{
+		return 0;
+	}
 
 }
