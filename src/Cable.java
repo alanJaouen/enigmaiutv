b5d84c4a09e89355c1e6cefee79edf50b194cdaa
corentin-dem <corentin-dem@44c46b43-e275-4c20-bec9-bf31a1ff7110>
2016-03-07 17:09:33 +0000

    class Cable + debut Rotor

--- src/Cable.java
+++ src/Cable.java
@@ -1,15 +1,63 @@
-import com.modeliosoft.modelio.javadesigner.annotations.objid;
 
-@objid ("3d6f3486-0c49-45ee-8a8a-e99020df6fa4")
-public class Cable {
-    @objid ("0f8b51aa-a0d2-48f0-afd7-d3b0cce34425")
+public class Cable 
+{
     public char lettre1;
 
-    @objid ("b1b28d30-4509-482d-98a5-cbb811736266")
     public char lettre2;
 
-    @objid ("893cdb66-7f3d-40b7-95d9-425828029b61")
-    public char encode(char c) {
+    public Cable()
+    {
+    	this.lettre1 = ' ';
+    	this.lettre2 = ' ';
+    }
+    
+    public Cable(char l1, char l2)
+    {
+    	this.lettre1 = l1;
+    	this.lettre2 = l2;
+    }
+    
+    public void setLettre1(char l)
+    {
+    	this.lettre1 = l;
+    }
+    
+    public void setLettre2(char l)
+    {
+    	this.lettre2 = l;
+    }
+    
+    public char getLettre1()
+    {
+    	return this.lettre1;
+    }
+    
+    public char getLettre2()
+    {
+    	return this.lettre2;
+    }
+    
+    public char encode(char c) 
+    {
+    	if(c==this.lettre1)
+    		return this.lettre2;
+    	if(c==this.lettre2)
+    		return this.lettre1;
+    	return c;
     }
 
+    public boolean equals(Cable c)
+    {
+    	if (this == c)
+    		if(this.lettre1 == c.lettre1)
+    			if(this.lettre2 == c.lettre2)
+    				return true;
+    	return false;
+    }
+    
+	@Override
+	public String toString() {
+		return "Cable [lettre1=" + lettre1 + ", lettre2=" + lettre2 + "]";
+	}
+    
 }
