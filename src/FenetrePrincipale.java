d3487793d5340ab50d006af35ee7a0fea4929712
Alan Jaouen <alanJaouen@users.noreply.github.com>
2016-03-09 22:10:58 +0000

    modification de l'ihm

--- src/FenetrePrincipale.java
+++ src/FenetrePrincipale.java
@@ -2,12 +2,16 @@ import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Component;
 import java.awt.Container;
+import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.GridLayout;
+import java.awt.Image;
 import java.awt.TextArea;
 import java.awt.Toolkit;
 
 import javax.swing.BorderFactory;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
@@ -33,11 +37,13 @@ public class FenetrePrincipale extends JFrame {
 		super("IHM cool");
 		int w = (int) Toolkit.getDefaultToolkit().getScreenSize().getWidth();
 		int h = (int) Toolkit.getDefaultToolkit().getScreenSize().getHeight();
-		this.setBounds(w/10, h/10, (int)(w/1.5), (int)(h/1.5));
+		this.setBounds(0, 0, (int)((h/1.5)*1.7), (int)(h/1.5));
 		this.pane=this.getContentPane();
 		
 		this.pane.add(this.getJPanel());
 		
+		
+		this.setResizable(false);
 		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
 		this.setVisible(true);
 		
@@ -56,28 +62,86 @@ public class FenetrePrincipale extends JFrame {
 		p.setLayout(new GridLayout(2,1));
 		p.add(this.getPanelRotor());
 		p.add(this.getPanelCable());
-		p.setBackground(Color.green);
 		return p;
 	}
 
 	private Component getPanelCable() {
 		JPanel p = new JPanel();
+		p.setLayout(new GridLayout(5,1));
 		p.setBorder(BorderFactory.createTitledBorder("Réglage des cables"));
+		
+		
+		//haut
+		JPanel cable1= new JPanel();
+		JLabel c11= new JLabel("M");//TODO variable de classe + methode paint de p l.70
+		JLabel c12= new JLabel("N");//TODO mangey
+		
+		cable1.add(c11);
+		cable1.add(new JLabel("Cable 1"));
+		cable1.add(c12);
+		p.add(cable1);
+		//millieu
+		char[][] tabClavier= {
+			{'A', 'Z','E','R','T','Y','U','I','O','P'},
+			{'q', 's','d','f','g','h','j','k','l','m'},
+			{' ', ' ','w','x','c','v','b','n',' ',' '}
+			};
+		
+		
+		JPanel[] panelTouches= new JPanel[3];
+		
+		
+		
+		for(int j=0; j<3; j++)
+		{
+			panelTouches[j]=new JPanel();
+			panelTouches[j].setLayout(new GridLayout(1,10,5,5));
+			for(char i: tabClavier[j])
+			{
+				JLabel t=new JLabel(i+"");
+				t.setHorizontalAlignment(JTextField.CENTER);
+				panelTouches[j].add(t);
+			}
+			p.add(panelTouches[j]);
+		}
+		//bas
+		p.add(new JLabel());
 		return p;
 	}
 
 	private Component getPanelRotor() {
-		JPanel p = new JPanel();
+		JPanel p = new JPanel()
+			{
+				private static final long serialVersionUID = 1L;
+
+				public void paint(Graphics g)
+				{
+					int w = (int) Toolkit.getDefaultToolkit().getScreenSize().getWidth()/2;
+					int h = (int) Toolkit.getDefaultToolkit().getScreenSize().getHeight()/2;
+					super.paint(g);
+					Image img = new ImageIcon("./img/rotor.png").getImage();
+					for(int i=0;i<3;i++)
+						g.drawImage(img, 10+i*(h/3 +h/25), h/11+h/80, h/3,h/3, null);
+	
+					
+				}
+			};
+			
+			
 		p.setBorder(BorderFactory.createTitledBorder("Réglage des rotors"));
 		p.setLayout(new GridLayout(1,3));
 		this.tabRotor = new JTextField[3];
-		p.setBackground(Color.pink);
 		for (int i=0; i<3; i+=1)
 		{
-			JTextField l= new JTextField("i="+i); //todo initialiser avec 1ere valeur du rotor
+			JTextField l= new JTextField(); //todo initialiser avec 1ere valeur du rotor
 			JPanel pint= new JPanel();
+			JPanel centre= new JPanel();
+			centre.add(l);
 			this.tabRotor[i]=l;
+			
 			l.setEnabled(true);
+			l.setPreferredSize(new Dimension( 25, 25 ));
+			l.setHorizontalAlignment(JTextField.CENTER);
 			pint.setLayout(new GridLayout(3,3));
 			pint.setOpaque(false);
 			
@@ -85,7 +149,7 @@ public class FenetrePrincipale extends JFrame {
 			{
 				if(j==4)
 				{
-					pint.add(l);
+					pint.add(centre);
 					continue;
 				}
 				JLabel k= new JLabel("")
@@ -133,12 +197,21 @@ public class FenetrePrincipale extends JFrame {
 		if(ediatable)
 		{
 			titre="Entrée";
-			this.tabTextArea[0]=t1; //TODO ajouter Listener
+			this.tabTextArea[0]=t1;
+			JButton valider = new JButton("Valider");//TODO ajouter listener
+			JButton fichier = new JButton("Lire un fichier");//TODO ajouter listener
+			
+			JPanel panelbouton= new JPanel();
+			panelbouton.setLayout(new GridLayout(1,2));
+			panelbouton.add(fichier);
+			panelbouton.add(valider);
+			
+			p.add(panelbouton, BorderLayout.SOUTH);
 		}
 		else
 		{
 			titre="Sortie";
-			this.tabTextArea[1]=t1; //TODO ajouter Listener
+			this.tabTextArea[1]=t1;
 		}
 		
 		p.setBorder(BorderFactory.createTitledBorder(titre));
