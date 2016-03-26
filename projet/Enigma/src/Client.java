cf2e2ac9fa0a7a5f3682e15a4bd8d438b820362a
corentin-dem <corentin-dem@44c46b43-e275-4c20-bec9-bf31a1ff7110>
2016-03-26 18:10:33 +0000

    mise de la fonction parcours_dico dans la classe enigma + decodage sans connaitre les reglages fini

--- projet/Enigma/src/Client.java
+++ projet/Enigma/src/Client.java
@@ -1,89 +1,14 @@
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
-import java.util.ArrayList;
-
 
 public class Client 
 {
-	private BufferedReader br;
-
 	public static void main(String args[])
 	{
-		Client bob=new Client();
-		bob.parcours_dico("bla le petit chien dans la petite maison");
+		//Client bob=new Client();
+		//bob.parcours_dico("bla le petit chien dans la petite maison");
 	}
-		public Client() {
-		super();
 	
+	public Client() 
+	{
+		super();
 	}
-
-		
-///////////////////////////////////////////////////////////////////////////////////////
-		int parcours_dico(String s) 
-		{	
-			int nb_occurence=0;
-			
-			ArrayList<String> dico= new ArrayList<String>();
-			
-					// ACCES AU FICHIER DICO
-					
-					try {
-						br = new BufferedReader(new FileReader("dico.txt"));
-					} catch (FileNotFoundException e1) {
-						e1.printStackTrace();
-						System.out.println("Impossible de trouver le fichier");
-					}
-					
-					// RECUPERATION DES MOTS
-			        String mot = "";      
-			            	
-					try {
-						
-						while(mot!= null){
-							
-							dico.add(mot);
-							mot=br.readLine();
-						}
-						
-					} catch (IOException e) {
-						e.printStackTrace();System.out.println("Impossible de lire le fichier");
-					}
-					 
-					
-					// AFFICHEGE DONNEES 
-					
-					for(int i=0; i<dico.size();i++)
-					{
-					System.out.println(dico.get(i));
-					}
-					
-					// SPLIT DE S
-					
-					 String str[]= s.split(" ");
-					 
-					 for(int j=0;j<str.length;j++)
-					 {
-					 System.out.println(str[j]);
-					 }
-					 
-					 // CALCUL DU NB D OCCURENCE
-					 
-			
-					 for(int l=0; l<str.length;l++)
-					 {
-						 System.out.println("2");
-						
-						 if(dico.contains(str[l]))
-						 {
-							 System.out.println("3--------");
-							 nb_occurence++;
-						 }
-						 
-					 }	 
-						 
-					 System.out.println(nb_occurence);
-						return nb_occurence;
-		}
 }
--- projet/Enigma/src/Composant.java
+++ projet/Enigma/src/Composant.java
@@ -1,35 +1,26 @@
-import com.modeliosoft.modelio.javadesigner.annotations.objid;
 
-@objid ("2dd05c17-8e8a-46b6-94ce-bcba77ce1649")
 public abstract class Composant {
-    @objid ("bfa25dbc-d081-470c-97cf-7392ef1c5cea")
     private Composant composantSuivant;
 
-    @objid ("2dd75901-1e6f-4652-a70d-4da012c9ab56")
     private Composant composantPrecedant;
 
-    @objid ("ff8ad837-e66b-4a4c-9569-f714093b849b")
     public abstract char encoder(char c, int sens);
 
-    @objid ("0103d81a-7aeb-4da5-a237-0459583d5ce2")
     public Composant getComposantSuivant() {
         // Automatically generated method. Please delete this comment before entering specific code.
         return this.composantSuivant;
     }
 
-    @objid ("7a13d86c-ef51-4204-b6ba-f4ea79c7d0b1")
     public void setComposantSuivant(Composant value) {
         // Automatically generated method. Please delete this comment before entering specific code.
         this.composantSuivant = value;
     }
 
-    @objid ("e72c0d19-fd30-49a8-a09b-852968364d7e")
     public Composant getComposantPrecedant() {
         // Automatically generated method. Please delete this comment before entering specific code.
         return this.composantPrecedant;
     }
 
-    @objid ("fcd34df5-ead6-4dcc-856a-b7fb348b738b")
     public void setComposantPrecedant(Composant value) {
         // Automatically generated method. Please delete this comment before entering specific code.
         this.composantPrecedant = value;
